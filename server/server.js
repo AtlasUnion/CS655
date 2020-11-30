16ef9c33c98724657c8221b9e0498a1e60ef827d
Ryan Ruan <zruan@bu.edu>
2020-11-30 17:19:48 -0500

    Update server to send message to worker and provide the capbability to add/remove worker

--- server/server.js
+++ server/server.js
@@ -40,8 +40,11 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var net = require("net");
 var carrier = require("carrier");
+var readline = require("readline");
 var worker_ips = ["127.0.0.1"];
-var worker_port = 1337;
+var num_worker_to_use = 1;
+var worker_port = 1337; // TODO: change actual port
+var total_search_space = Math.pow(52, 5);
 var app = express();
 app.listen(8080, function () {
     console.log("Server listening...");
@@ -69,18 +72,26 @@ function sendRequest(md5hash) {
         return __generator(this, function (_a) {
             beginTime = Date.now();
             return [2 /*return*/, new Promise(function (resolve, reject) {
-                    for (var _i = 0, worker_ips_1 = worker_ips; _i < worker_ips_1.length; _i++) {
-                        var ip = worker_ips_1[_i];
+                    var length_of_search_for_each_worker = total_search_space / num_worker_to_use;
+                    for (var i = 0; i < num_worker_to_use; i++) {
+                        var worker_json = {
+                            hash: md5hash,
+                            index: [i * length_of_search_for_each_worker, (i + 1) * length_of_search_for_each_worker]
+                        };
+                        if (i == num_worker_to_use - 1) {
+                            worker_json.index = [i * length_of_search_for_each_worker, total_search_space];
+                        }
                         var socket = new net.Socket();
-                        socket.connect(worker_port, ip, function () { console.log("Connected"); });
+                        socket.connect(worker_port, worker_ips[i], function () { });
+                        socket.write(JSON.stringify(worker_json) + "\n");
                         var my_carrier = carrier.carry(socket);
                         my_carrier.on('line', function (line) {
-                            if (line == "Fail to find password") { // replace with something cannot be password
+                            if (line == "Fail to find password") {
                                 socket.destroy();
                             }
                             else {
                                 var totalTime = Date.now() - beginTime;
-                                console.log("Total time:" + totalTime);
+                                console.log(totalTime);
                                 resolve(line);
                             }
                         });
@@ -89,3 +100,22 @@ function sendRequest(md5hash) {
         });
     });
 }
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+var changeNumofWorkers = function () {
+    rl.question("Number of workers to use?", function (num) {
+        var requested_num_worker_to_use = parseInt(num);
+        if (isNaN(requested_num_worker_to_use)) {
+        }
+        else if ((requested_num_worker_to_use < 1) || (requested_num_worker_to_use > worker_ips.length)) {
+        }
+        else {
+            num_worker_to_use = requested_num_worker_to_use;
+        }
+        console.log("Current:" + num_worker_to_use);
+        changeNumofWorkers();
+    });
+};
+changeNumofWorkers();
