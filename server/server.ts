b9887daeb009a58db393431270f0622e802dc133
Hannah Catabia <hannahcatabia@gmail.com>
2020-12-06 23:17:35 -0500

    1000

--- server/server.ts
+++ server/server.ts
@@ -11,7 +11,7 @@ var worker_ips = ["10.10.0.1", "10.10.1.1", "10.10.2.1", "10.10.3.1", "10.10.4.1
 var num_worker_to_use = 1
 const worker_port = 1338
 const total_search_space = 52**5
-const num_of_pieces = 500
+const num_of_pieces = 1000
 
 const app = express()
 app.listen(8080, () => {
