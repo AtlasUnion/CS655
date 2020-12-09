1f8e4b2735df77258fd83c414da3fd8dd81b1bb2
Ryan Ruan <zruan@bu.edu>
2020-12-09 10:19:11 -0500

    Delete

--- load_test/load_test.js
+++
@@ -1,9 +0,0 @@
-// use the following code in a browser console or node
-
-function y() {
-    const data = new URLSearchParams(); 
-    data.append('md5hash', '9qYmMWfJLehkSsmYs8Tk0Q'); // TODO: replace with actual hash
-    fetch('/api/processform', {method: 'post', body: data})
-}
-
-setInterval(y, 1000)
